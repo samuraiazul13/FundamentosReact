b894e47a027d443c847c15d53d7cf4c427b4c25b
samuraiazul13 <felipeob98@gmail.com>
2026-07-27 13:24:15 -0500

    componentes 1

--- FundamentosReact/src/App.jsx
+++ FundamentosReact/src/App.jsx
@@ -1,37 +1,49 @@
-import { useState } from 'react'
-import foto from './assets/foto.png'
-import './App.css'
+import { useState } from "react"
+import foto from "./assets/foto.png"
+import Header from "./components/header"
+import Perfil from "./components/perfil"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <div className="contenedor">
+        <Header/>
+        <Perfil/>
+      </div>
+
       <div className="tarjeta-cv">
-      <h1>ANALISIS Y DESARROLLO DE SOFTWARE</h1>
-      <h2>TECNOLOGO</h2>
-      <p>HOJA DE VIDA APRENDIZ</p>
-      <img src={foto} alt="Foto de inicio" width="250"/>
-      <h2>LUIS FELIPE GUERRERO TRUJILLO</h2>
-      <h2>29</h2>
-      <h2>BOGOTÁ DC</h2>
-      <h2>felipeob98@gmail.com</h2>
-      <h2>ADSO</h2>
-      <h1>CURSOS REALIZADOS</h1>
-      <h2>Seguridad  y Salud en  el Trabajo</h2>
-      <h2>Japonés</h2>
-      <h1>OBJETIVO PROFESIONAL</h1>
-      <h2>¿Por qué desea aprender React?</h2><br></br>
-      <p>Porque ofrece alta demanda laboral, 
-        un diseño basado en componentes reutilizables y la versatilidad de 
-        crear apps web y móviles</p><br></br>
-      <h2>¿En qué tipo de proyectos le gustaría utilizarlo?</h2>
-      <p>Proyectos prácticos como una plataforma de comercio electrónico (E-commerce),
-        un tablero de gestión de tareas (tipo Trello) o una aplicación</p>
-      <button type="button">Conoce mas sobre mí</button>
-     </div>
+        <h1>ANALISIS Y DESARROLLO DE SOFTWARE</h1>
+        <h2>TECNOLOGO</h2>
+        <img src={foto} alt="Foto de inicio" width="250" />
+        <h2>LUIS FELIPE GUERRERO TRUJILLO</h2>
+        <h2>29</h2>
+        <h2>BOGOTÁ DC</h2>
+        <h2>felipeob98@gmail.com</h2>
+        <h2>ADSO</h2>
+        <h1>CURSOS REALIZADOS</h1>
+        <h2>Seguridad y Salud en el Trabajo</h2>
+        <h2>Japonés</h2>
+        <h1>OBJETIVO PROFESIONAL</h1>
+        <h2>¿Por qué desea aprender React?</h2>
+        <br></br>
+        <p>
+          Porque ofrece alta demanda laboral, un diseño basado en componentes
+          reutilizables y la versatilidad de crear apps web y móviles
+        </p>
+        <br></br>
+        <h2>¿En qué tipo de proyectos le gustaría utilizarlo?</h2>
+        <p>
+          Proyectos prácticos como una plataforma de comercio electrónico
+          (E-commerce), un tablero de gestión de tareas (tipo Trello) o una
+          aplicación
+        </p>
+        <button type="button">Conoce mas sobre mí</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FundamentosReact/src/components/header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+return(
+    <header>
+        <h1>HOJA DE VIDA APRENDIZ</h1>
+    </header>
+
+)
+}
+export default Header
